f6e1cd7500449ab53322f8788a405030f319ad05
Michael Little <michael.little@xero.com>
2023-10-29 22:10:06 +1300

    Back to basic express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('asdasd');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/ping', (_: Request, res: Response) => {
+    res.status(200).send('Alive!')
+})
+
+
+console.log('Running on port 3001');
+app.listen(3001);
