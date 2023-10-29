6ebfa0a616df894c00d28d0bc63b7c8d136b49ab
Michael Little <michael.little@xero.com>
2023-10-29 22:15:20 +1300

    Console colours

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import { cyan } from "colors/safe";
 
 const app = express();
 
-app.get('/ping', (_: Request, res: Response) => {
-    res.status(200).send('Alive!')
-})
+app.get("/ping", (_: Request, res: Response) => {
+  res.status(200).send("Alive!");
+});
 
-
-console.log('Running on port 3001');
+console.log(cyan("Running on port 3001"));
 app.listen(3001);
