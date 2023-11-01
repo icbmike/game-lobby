0ff4a18b78fcbe46b19f460781193b8c0e54d4fe
Michael Little <michael.francis.little@gmail.com>
2023-11-01 23:17:17 +1300

    Add request loggin middleware

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { cyan } from "colors/safe";
 import { configureHandlers } from "./endpoints/lobbies";
 import { startJob } from "./lobbyExpirationJob";
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get("/ping", (req, res) => {
   res.status(200).send("Alive!");
